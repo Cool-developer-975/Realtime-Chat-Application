866b0efc579f322f08cf2fc0e09b7166441c17ae
Ameya Kawade <ameyakawade0@gmail.com>
2024-04-01 14:17:55 +0530

    Made some minor changes

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,11 @@
 const {Pool} = require("pg");
 
 const pool = new Pool({
-    user:"postgres",
-    host:"localhost",
-    password:"Ameya",
-    database:"Real Time Chat Application",
-    port:5432
+    user:process.env.POSTGRES_USER,
+    host:process.env.POSTGRES_HOST,
+    password:process.env.POSTGRES_PASSWORD,
+    database:process.env.POSTGRES_DATABASE,
+    port:process.env.POSTGRES_PORT
 });
 
 
